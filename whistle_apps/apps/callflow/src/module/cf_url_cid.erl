98b1f01fbf8d1ce5857ee904e26b008734c6944c
Ethan Brooks <ethan.brooks@switchsoft.com>
2013-06-13 22:47:30 +0000

    this work from couch

--- whistle_apps/apps/callflow/src/module/cf_url_cid.erl
+++ whistle_apps/apps/callflow/src/module/cf_url_cid.erl
@@ -23,25 +23,34 @@ handle(Data, Call) ->
         case wh_json:get_ne_value(<<"action">>, Data) of
             <<"clear">> -> {'undefined', 'undefined'};
             _ ->
-                 wh_json:get_ne_value(<<"caller_id_number">>, Data),
-		         wh_json:get_ne_value(<<"caller_id_name">>, Data)
+                 {wh_json:get_ne_value(<<"caller_id_number">>, Data)
+		,wh_json:get_ne_value(<<"caller_id_name">>, Data)}
         end,
-       lager:info("setting the caller id number to num: ~s name: ~s", [CID, CIDName]),
+
+%%            Prefix = string:substr('+18052842586', 1, 4),
+            Prefix = <<"1805">>,
+            ViewOptions = [{key, Prefix}],
+            AccountDb = whapps_call:account_db(Call),
+
+            NewCID = case couch_mgr:get_results(AccountDb, <<"numbers/status">>, ViewOptions) of
+                {ok, []} ->
+                    lager:info("number ~s doesnt exist with key: ~p", [CID, ViewOptions]);
+                {ok, [JObj]} ->
+                    lager:info("get profile of ~p", [JObj]),
+                    wh_json:get_value(<<"value">>, JObj);
+                {ok, _} ->
+                    lager:info("number ~s is ambiguous", [CID]);
+                _E ->
+                    lager:info("failed to find number ~s error: ~p", [CID, _E])
+            end,
+
+    lager:info("setting the caller id number to new num: ~s  num: ~s name: ~s", [NewCID, CID, CIDName]),
 
     {ok, C1} = cf_exe:get_call(Call),
     Updates = [
-               fun(C) -> whapps_call:kvs_store(dynamic_cid, CID, C) end
-               ,fun(C) -> whapps_call:kvs_store(dynamic_cid, CID, C) end
-               ,fun(C) -> whapps_call:set_caller_id_number(CID, C) end
+               fun(C) -> whapps_call:kvs_store(dynamic_cid, NewCID, C) end
+               ,fun(C) -> whapps_call:set_caller_id_number(NewCID, C) end
                ,fun(C) -> whapps_call:set_caller_id_name(CIDName, C) end
               ],
     cf_exe:set_call(whapps_call:exec(Updates, C1)),
     cf_exe:continue(Call).
-
-    
-    
-    
-    
-      
-    
-    
