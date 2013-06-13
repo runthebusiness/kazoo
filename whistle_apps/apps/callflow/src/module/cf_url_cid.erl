7a4b10829e734b822a1838edd2bc104f766d966c
root <root@neat.switchsoft.com>
2013-06-13 10:30:28 -0700

    added query with no action

--- whistle_apps/apps/callflow/src/module/cf_url_cid.erl
+++ whistle_apps/apps/callflow/src/module/cf_url_cid.erl
@@ -26,7 +26,21 @@ handle(Data, Call) ->
                  wh_json:get_ne_value(<<"caller_id_number">>, Data)
 		,wh_json:get_ne_value(<<"caller_id_name">>, Data)
         end,
-
+        
+		CID = try wh_util:to_integer(CID) catch _:_ -> 0 end,
+		%% find the touchCID from the CID set in the callflow.  
+            ViewOptions = [{key, CID}],
+            AccountDb = whapps_call:account_db(Call),
+            case couch_mgr:get_results(AccountDb, <<"numbers/status">>, ViewOptions) of
+                {ok, []} ->
+                    lager:info("number ~s doesnt exist", [CID]);
+                {ok, [JObj]} ->
+                    lager:info("get profile of ~p", [JObj]);
+                {ok, _} ->
+                    lager:info("number ~s is ambiguous", [CID]);
+                _E ->
+                    lager:info("failed to find number ~s: ~p", [CID, _E])
+            end,
     lager:info("setting the caller id number to num: ~s name: ~s", [CID, CIDName]),
 
     {ok, C1} = cf_exe:get_call(Call),
@@ -38,3 +52,11 @@ handle(Data, Call) ->
               ],
     cf_exe:set_call(whapps_call:exec(Updates, C1)),
     cf_exe:continue(Call).
+
+    
+    
+    
+    
+      
+    
+    
