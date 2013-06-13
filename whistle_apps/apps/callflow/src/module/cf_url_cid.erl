03d2cae921e4b9af34490f395f06b298708ed660
Ethan Brooks <ethan.brooks@switchsoft.com>
2013-06-13 11:34:38 -0700

    Reverted to working

--- whistle_apps/apps/callflow/src/module/cf_url_cid.erl
+++ whistle_apps/apps/callflow/src/module/cf_url_cid.erl
@@ -23,25 +23,10 @@ handle(Data, Call) ->
         case wh_json:get_ne_value(<<"action">>, Data) of
             <<"clear">> -> {'undefined', 'undefined'};
             _ ->
-                 wh_json:get_ne_value(<<"caller_id_number">>, Data)
-		,wh_json:get_ne_value(<<"caller_id_name">>, Data)
+                 wh_json:get_ne_value(<<"caller_id_number">>, Data),
+		         wh_json:get_ne_value(<<"caller_id_name">>, Data)
         end,
-        
-		CID = try wh_util:to_integer(CID) catch _:_ -> 0 end,
-		%% find the touchCID from the CID set in the callflow.  
-            ViewOptions = [{key, CID}],
-            AccountDb = whapps_call:account_db(Call),
-            case couch_mgr:get_results(AccountDb, <<"numbers/status">>, ViewOptions) of
-                {ok, []} ->
-                    lager:info("number ~s doesnt exist", [CID]);
-                {ok, [JObj]} ->
-                    lager:info("get profile of ~p", [JObj]);
-                {ok, _} ->
-                    lager:info("number ~s is ambiguous", [CID]);
-                _E ->
-                    lager:info("failed to find number ~s: ~p", [CID, _E])
-            end,
-    lager:info("setting the caller id number to num: ~s name: ~s", [CID, CIDName]),
+       lager:info("setting the caller id number to num: ~s name: ~s", [CID, CIDName]),
 
     {ok, C1} = cf_exe:get_call(Call),
     Updates = [
